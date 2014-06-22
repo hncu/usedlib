f3469e862842a4b424cd35bfe1c3ec96c2db8623
wanderknight <wanderknight@gmail.com>
2014-06-22 16:04:14 +0800

    add shiro test & database connection

--- usedlib/grails-app/conf/BootStrap.groovy
+++ usedlib/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,70 @@
+import grails.util.GrailsUtil
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+	def shiroSecurityService
+
+	def init = { servletContext ->
+		switch (GrailsUtil.environment) {
+
+			case  "development":
+				createTestingUsers()
+			//				createTestingCategorys()
+			//				createTestingBooks()
+			//				createTestingOwns()
+			//				createTestingBorrows()
+
+				break;
+
+			case "production":
+				println "No special configuration required"
+				break;
+		}
+	}
+	def destroy = {
+	}
+
+	void createTestingUsers(){
+
+		// Create the admin role
+		def adminRole = ShiroRole.findByName('ROLE_ADMIN') ?:
+				new ShiroRole(name: 'ROLE_ADMIN').save(flush: true, failOnError: true)
+
+		// Create the user role
+		def userRole = ShiroRole.findByName('ROLE_USER') ?:
+				new ShiroRole(name: 'ROLE_USER').save(flush: true, failOnError: true)
+
+		// Create an admin user
+		def adminUser = ShiroUser.findByUsername('admin') ?:
+				new ShiroUser(username: "admin",
+				passwordHash: shiroSecurityService.encodePassword('password'))
+				.save(flush: true, failOnError: true)
+
+		// Add roles to the admin user
+		assert adminUser.addToRoles(adminRole)
+		.save(flush: true, failOnError: true)
+		//.addToRoles(userRole)
+
+		// Create an standard user
+		def standardUser = ShiroUser.findByUsername('joe') ?:
+				new ShiroUser(username: "joe",
+				passwordHash: shiroSecurityService.encodePassword('password'))
+				.save(flush: true, failOnError: true)
+
+		// Add role to the standard user
+		assert standardUser.addToRoles(userRole)
+		.save(flush: true, failOnError: true)
+
+		for(i in 0..3) {
+			def jane = ShiroUser.findByUsername("knight${i}") ?:
+					new ShiroUser(username:"knight${i}",
+					passwordHash:shiroSecurityService.encodePassword('123456'))
+					.save(flush: true, failOnError: true)
+			if(jane.hasErrors()){
+				println jane.errors
+			}
+			assert jane.addToRoles(userRole)
+			.save(flush: true, failOnError: true)
+		}
+	}
 }
--- usedlib/grails-app/domain/ShiroUser.groovy
+++ usedlib/grails-app/domain/ShiroUser.groovy
@@ -1,10 +1,13 @@
 class ShiroUser {
     String username
     String passwordHash
-    
+	String email
+	
     static hasMany = [ roles: ShiroRole, permissions: String ]
 
     static constraints = {
         username(nullable: false, blank: false, unique: true)
+		passwordHash(nullable: false, blank: false,size:6..70)
+		email(nullable:true,blank:false,unique:true,email: true)
     }
 }
